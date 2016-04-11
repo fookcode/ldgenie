2692b683313432532eabae3ac82b681d3454727e
kinee <kinee@x201s>
2016-04-11 22:13:07 +0800

    4.11

---
+++ app/src/main/java/com/vrv/ldgenie/service/GenieService.java
@@ -0,0 +1,36 @@
+package com.vrv.ldgenie.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by kinee on 2016/4/11.
+ */
+public class GenieService extends Service {
+    public GenieService() {
+        super();
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
