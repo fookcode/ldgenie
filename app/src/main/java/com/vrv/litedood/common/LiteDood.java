c060ff40ab42c9d0c7e66a9a46eb99c870035fc1
kinee <kinee@x201s>
2016-04-19 21:37:00 +0800

    4.19

--- app/src/main/java/com/vrv/litedood/common/LiteDood.java
+++ app/src/main/java/com/vrv/litedood/common/LiteDood.java
@@ -1,11 +1,11 @@
 package com.vrv.litedood.common;
 
-import com.vrv.litedood.LiteDoodApplication;
-
 /**
  * Created by kinee on 2016/4/18.
  */
 public final class LiteDood {
+
+    public final static String APP_ID = "litedood";
 
     public static final String SCHEME = "content://";
 
@@ -13,5 +13,5 @@ public final class LiteDood {
 
     public static final String URI = SCHEME + AUTHORITY;
 
-    public static final String DB_NAME = LiteDoodApplication.APP_ID + ".db";
+    public static final String DB_NAME = APP_ID + ".db";
 }
--- app/src/main/java/com/vrv/litedood/liteDoodApplication.java
+++ app/src/main/java/com/vrv/litedood/liteDoodApplication.java
@@ -3,19 +3,20 @@ package com.vrv.litedood;
 import android.app.Application;
 
 import com.vrv.imsdk.VIMClient;
+import com.vrv.litedood.common.LiteDood;
 
 /**
  * Created by kinee on 2016/3/24.
  */
 public class LiteDoodApplication extends Application {
-    public final static String APP_ID = "litedood";
+
     private static LiteDoodApplication appContext;
 
     @Override
     public void onCreate() {
         super.onCreate();
         appContext = this;
-        VIMClient.init(appContext, "litedood");
+        VIMClient.init(appContext, LiteDood.APP_ID);
     }
 
     public static LiteDoodApplication getAppContext() {
