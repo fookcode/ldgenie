aba72156068c6b82fff823d72a25754b07b4b47d
kinee <kinee@x201s>
2016-05-23 22:14:43 +0800

    5.23

--- app/src/main/java/com/vrv/litedood/common/widget/LitedoodMessageListViewCompat.java
+++ app/src/main/java/com/vrv/litedood/common/widget/LitedoodMessageListViewCompat.java
@@ -1,15 +1,22 @@
 package com.vrv.litedood.common.widget;
 
 import android.content.Context;
+import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.ListViewCompat;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.MotionEvent;
+import android.view.Window;
 
 /**
  * Created by kinee on 2016/5/21.
  */
 public class LitedoodMessageListViewCompat extends ListViewCompat {
+    public final static String TAG = LitedoodMessageListViewCompat.class.getSimpleName();
     public LitedoodMessageListViewCompat(Context context) {
         super(context);
     }
@@ -29,5 +36,26 @@ public class LitedoodMessageListViewCompat extends ListViewCompat {
         if (h<oldh) {
             this.setSelection(this.getCount() -1);
         }
+
+        float g = getResources().getDisplayMetrics().density;
+        Log.v(TAG, "density: " + String.valueOf(g));
+        float g1 = (float)getResources().getDisplayMetrics().densityDpi / DisplayMetrics.DENSITY_DEFAULT;
+        Log.v(TAG, "densityDpi / Density_Default: " + String.valueOf(g1));
+
+        Rect rect= new Rect();
+        getWindowVisibleDisplayFrame(rect);
+
+        int contentViewHeight = ((AppCompatActivity)getContext()).getWindow().findViewById(Window.ID_ANDROID_CONTENT).getHeight();//顶层GroupView的高度，不含status和title，此例中也就是ListView的父对象
+        Log.v(TAG, "Android Content View height: " + String.valueOf(contentViewHeight));
+
+        if (((AppCompatActivity)getContext()).getWindow().getDecorView() == this.getRootView()) {
+            Log.v(TAG, "getWindow().getDecorView() == View.getRootView()");
+        }
+        int keyboardHeight = this.getRootView().getHeight() - (((LinearLayoutCompat)this.getParent()).getHeight() + rect.top);
+        float keyboardHeightDP = keyboardHeight / g;
+        Log.v(TAG, "dp: " + String.valueOf(keyboardHeightDP));
+
+
+
     }
 }
